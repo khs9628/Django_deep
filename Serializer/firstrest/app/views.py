2927e51494d3891588ef3a3dec9df8e57749aaeb
khs9628 <rlagus0908@naver.com>
2019-09-09 22:24:17 +0900

    deep

---
+++ Serializer/firstrest/app/views.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets
+from .models import Post
+from .serializer import PostSerializer
+
+#CBV
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
